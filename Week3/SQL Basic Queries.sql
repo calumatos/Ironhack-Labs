ef88e6d32ded433ec82a4484222f82317fe32205
Carmen Matos <calumatos@gmail.com>
2023-10-31 20:46:39 +0100

    Lab W3D2

---
+++ Week3/SQL Basic Queries.sql
@@ -0,0 +1,82 @@
+#### SQL Basic Queries lab ####
+
+USE sakila;
+
+# 1. Show all tables: 
+SHOW TABLES;
+
+# 2. Retrieve all the data from the tables actor, film and customer.
+SELECT *
+FROM actor;
+
+SELECT *
+FROM film;
+
+SELECT *
+FROM customer;
+
+# 3. Retrieve the following columns from their respective tables:
+# 3.1 Titles of all films from the film table
+SELECT title as film_title
+FROM film;
+
+# 3.2 List of languages used in films, with the column aliased as language from the language table
+SELECT distinct name as language
+FROM language;
+
+# 3.3 List of first names of all employees from the staff table.
+SELECT first_name 
+FROM staff;
+
+# 4. Retrieve unique release years.
+SELECT distinct release_year
+FROM film;
+
+# 5. Counting records for database insights:
+# 5.1 Determine the number of stores that the company has.
+SELECT COUNT(store_id) as number_stores
+FROM store;
+
+# 5.2 Determine the number of employees that the company has.
+SELECT COUNT(staff_id) as number_staff
+FROM staff;
+
+# 5.3 Determine how many films are available for rent and how many have been rented.
+SELECT count(inventory_id) as available_for_rent
+FROM rental;
+
+# 5.4 Determine the number of distinct last names of the actors in the database.
+SELECT distinct last_name 
+FROM actor;
+
+# 6. Retrieve the 10 longest films.
+SELECT title, length
+FROM film 
+ORDER BY length DESC
+LIMIT 10;
+
+# 7. Use filtering techniques in order to:
+# 7.1 Retrieve all actors with the first name "SCARLETT".
+SELECT first_name, last_name 
+FROM actor
+WHERE first_name LIKE "%SCARLETT%";
+
+#### BONUS:
+
+# 7.2 Retrieve all movies that have ARMAGEDDON in their title and have a duration longer than 100 minutes.
+# Hint: use LIKE operator. More information here.
+SELECT *
+FROM film
+WHERE title LIKE "%ARMAGEDDON%" and (length > "100");
+
+# 7.3 Determine the number of films that include Behind the Scenes content
+SELECT COUNT(title) as number_films
+FROM film
+WHERE special_features LIKE "%scenes"
+GROUP BY special_features
+LIMIT 1;
+
+
+
+
+
